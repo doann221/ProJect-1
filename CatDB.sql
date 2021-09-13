84ab36a25af9ca4e4abba60eaceaa47487244927
doann221 <doann221@gmail.com>
2021-09-13 14:47:05 +0700

    update

--- CatDB.sql
+++ CatDB.sql
@@ -16,10 +16,21 @@ create table Staffs(
 
 create user if not exists 'vtca'@'localhost' identified by 'vtcacademy';
 grant all on CatDB.* to 'vtca'@'localhost';
-
-
-
-    insert into Staffs( user_name, user_pass, role) values
-    ( 'doann221', '44a86e166293f65a7a692150234cdef0', 1);
-    
+create table Cat(
+cat_id int auto_increment primary key not null,
+cat_name varchar(200) not null unique,
+cat_age int,
+cat_color varchar(100),
+cat_weight int,
+cat_longevety int,
+cat_quantity int,
+cat_price decimal
+);
+drop table Cat;
+insert into Staffs( user_name, user_pass, role) values
+( 'doann221', '44a86e166293f65a7a692150234cdef0', 1);
 select * from Staffs;
+
+insert into Cat(cat_name, cat_age, cat_color, cat_weight, cat_longevety, cat_quantity, cat_price) values
+('Mèo Anh lông dài', '3', 'trắng', '3', '14', '20', '4000000');
+select * from Cat;
