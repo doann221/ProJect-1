653b6f9ce987288db2e68ced3738817f8ebf0079
doann221 <doann221@gmail.com>
2021-09-13 19:35:49 +0700

    update

--- CatDB.sql
+++ CatDB.sql
@@ -4,7 +4,6 @@ create database CatDB;
 
 use CatDB;
 
-
 create table Staffs(
 	staff_id int auto_increment primary key,
     user_name varchar(100) not null unique,
@@ -19,18 +18,32 @@ grant all on CatDB.* to 'vtca'@'localhost';
 create table Cat(
 cat_id int auto_increment primary key not null,
 cat_name varchar(200) not null unique,
-cat_age int,
+cat_age varchar(50),
 cat_color varchar(100),
 cat_weight int,
-cat_longevety int,
+cat_longevety varchar(20),
 cat_quantity int,
 cat_price decimal
 );
-drop table Cat;
+-- drop table Staffs;
 insert into Staffs( user_name, user_pass, role) values
-( 'doann221', '44a86e166293f65a7a692150234cdef0', 1);
+( 'doann221', '7d669799ea755df1aa4dd7b92e443013', 1);
+-- pass: Catsale 123
 select * from Staffs;
 
+
 insert into Cat(cat_name, cat_age, cat_color, cat_weight, cat_longevety, cat_quantity, cat_price) values
-('Mèo Anh lông dài', '3', 'trắng', '3', '14', '20', '4000000');
+('Mèo Anh lông dài', '3 tháng', 'trắng', '3', '14 năm', '20', '4000000'),
+('Mèo Xiêm Thái', '4 tháng', 'xám hải cẩu' , 5 , '15 năm', 20 , 6000000),
+('Mèo Ba Tư Truyền Thống', '4 tháng ', 'trắng', 7, '10-17 năm', 20, 10000000),
+ ('Mèo Tai Cụp Scottish Fold' , '13 tháng', 'lông  ngắn xám', 5, '15 năm', 30 , 12000000);
+
+insert into Cat(cat_name, cat_age, cat_color, cat_weight, cat_longevety, cat_quantity, cat_price) values
+('Mèo Ba Tư lông ngắn','15 tháng', 'tam thể', 4, '20 năm', 25, 20000000),
+('Mèo Ba tư Himalaya', '16 tháng', 'nâu đốm', 3, '20 năm', 20, 15000000),
+('Mèo Chichilla', '17 tháng',  'silver' , 5, '20 năm' , 15, 10000000),
+('Giống Mèo Abyssinian', '7 tháng', 'nâu vàng', 5, '9-15 năm', 10, 25000000);   
 select * from Cat;
+
+
+ 
---
+++ Persistance/Cat.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Persistance
+{
+    public class Cat
+    {
+      public int CatID{set; get;}
+      public string CatName{set; get;}
+      public decimal CatPrice{set; get;}
+      public string CatAge{set; get;}
+      public string CatColor{set; get;}
+      public int CatWeight{set; get;}
+      public string CatLongevety{set; get;}
+      public int quantity{set; get;}  
+    }
+}
